fa2b13b2c400df141a89cb321832cde488183b85
mikew <waugh.e.michael@gmail.com>
2025-02-10 23:35:37 -0500

    updates

--- src/sim_trade_pattern_ek.jl
+++ src/sim_trade_pattern_ek.jl
@@ -15,9 +15,10 @@ function sim_trade_pattern_ek(λ, τ, θ, σ, code)
     # Draw productivities and and compute unit costs to produce each good in
     # each country
     Random.seed!(03281978 + code)
-    pconst = zeros(Ngoods, Ncntry)
+    pconst = Array{eltype(λ)}(undef, Ngoods, Ncntry)
+ 
 
-    for j in 1:Ncntry
+    Threads.@threads for j in 1:Ncntry
 
         u = rand(MersenneTwister(03281978 + j), Ngoods)
 
@@ -26,15 +27,15 @@ function sim_trade_pattern_ek(λ, τ, θ, σ, code)
     end
 
     # Loop to calculate the low price and country suppliers
-    m = zeros(Ncntry, Ncntry)
-    sum_price = zeros(Ncntry, 1)
-    rec_low_price = zeros(Ngoods, Ncntry)
+    m = Array{eltype(λ)}(undef,Ncntry, Ncntry)
+    sum_price = Array{eltype(λ)}(undef,Ncntry)
+    rec_low_price = Array{eltype(λ)}(undef,Ngoods, Ncntry)
 
     for gd in 1:Ngoods # This is the good
         
         for im in 1:Ncntry # Importing country
 
-            cif_price = zeros(Ncntry) 
+            cif_price = Array{eltype(λ)}(undef, Ncntry) 
 
             for ex in 1:Ncntry # This is the country (potentially) exporting the good
 
@@ -104,43 +105,56 @@ function sim_trade_pattern_ek_fast(λ, τ, θ, σ, code)
 
     # Parameters for technologies
     η = σ
-    inv_η = 1 / (1 - η)
-    inv_Ngoods = 1 / Ngoods
+    inv_η = 1.0 / (1 - η)
+    inv_Ngoods = 1.0 / Ngoods
 
     # Draw productivities and and compute unit costs to produce each good in
     # each country
-    Random.seed!(03281978 + code)
     
-    pconst = Array{Float64}(undef, Ngoods, Ncntry)
+    pconst = Array{Float64}(undef, Ncntry, Ngoods)
 
-    for j in 1:Ncntry
+    u = Array{Float64}(undef, Ncntry, Ngoods)
 
-        u = rand(MersenneTwister(03281978 + j), Ngoods)
+    rand!(MersenneTwister(03281978 + code ), u)
 
-        pconst[:, j] .= (log.(u) ./ (-λ[j])) .^ (1/θ) 
+    @time @fastmath u .= (-log.(u)).^ (1.0 / θ) 
+
+    #(log.(u) ./ (-λ[j])) .^ (1/θ) 
+
+    λ = λ.^ (-1.0 / θ)
+
+    @time @inbounds @views Threads.@threads for j in 1:Ncntry
+
+        pconst[j, :] .= u[j,:] .* λ[j] 
 
     end
 
     # Loop to calculate the low price and country suppliers
     m = zeros(Ncntry, Ncntry)
     sum_price = Array{Float64}(undef, Ncntry)
-    rec_low_price = Array{Float64}(undef, Ngoods, Ncntry)
+    rec_low_price = Array{Float64}(undef, Ncntry, Ngoods)
 
-    cif_price = Array{Float64}(undef, Ncntry)  # Preallocate outside loops
-    η_1 = 1 - η  # Precompute exponent
+    #cif_price = Array{Float64}(undef, 1)  # Preallocate outside loops
+    η_1 = 1.0 - η  # Precompute exponent
 
-    for gd in 1:Ngoods  # Loop over goods
-        for im in 1:Ncntry  # Loop over importing countries
+    @time Threads.@threads for gd in 1:Ngoods  # Loop over goods
 
-            low_price = Inf
-            min_ex = 1
+        @inbounds for im in 1:Ncntry  # Loop over importing countries
 
-            for ex in 1:Ncntry
-                cif_price[ex] = τ[ex, im] * pconst[gd, ex]
-                if cif_price[ex] < low_price
-                    low_price = cif_price[ex]
+            low_price = 1.0e7
+            min_ex = 1.0
+
+            @inbounds for ex in 1:Ncntry
+
+                cif_price = τ[ex, im] * pconst[ex, gd]
+
+                if cif_price < low_price
+
+                    low_price = cif_price
+                    
                     min_ex = ex
                 end
+
             end
 
             # Precompute power once
@@ -151,19 +165,21 @@ function sim_trade_pattern_ek_fast(λ, τ, θ, σ, code)
 
             # Update sum price and record lowest price
             sum_price[im] += low_price_pow
-            rec_low_price[gd, im] = low_price
+
+            rec_low_price[im, gd] = low_price
         end
+
     end
 
     # Loop to calculate aggregate price index and the trade shares.
 
-    for im in 1:Ncntry
+    @time for im in 1:Ncntry
 
         g_val = (sum_price[im] * inv_Ngoods)^inv_η
 
         for ex in 1:Ncntry
 
-            m[ex, im] = (inv_Ngoods*m[ex, im]) / g_val^(1 - η)
+            m[ex, im] = (inv_Ngoods*m[ex, im]) / g_val^(1.0 - η)
 
         end
 
